77761fa1369ea7ec8df9ba220b470d2920a94355
Krittin Kamkar <aomgg555@gmail.com>
2022-06-22 10:40:21 +0700

    new commit

--- App.js
+++ App.js
@@ -1,13 +1,18 @@
 import { View, Text,StyleSheet } from 'react-native'
 import React from 'react'
-import Logo from './components/Logo';
-import styles from './components/styles';
+//import Logo from './components/Logo';
+import styles from './components/styles'
+import StySheet from './components/stySheet';
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={[styles.title,styles.warning]}>Hello React Native</Text>
-      <Logo/>
+    <View >
+      {/* <Text style={[styles.title,styles.warning]}>Hello React Native</Text>
+       */}
+
+      <StySheet/>
       
+      
+           
     </View>
   )
 }
